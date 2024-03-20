a0889ee99cab1e0282a61ac2dd3597ac1fabe63d
Win Barua <win.barua2@gmail.com>
2024-03-20 21:01:03 +0000

    Setup CDK stack

--- server/cdk/cdk.go
+++ server/cdk/cdk.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/awss3"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awssns"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awssnssubscriptions"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
+	"github.com/joho/godotenv"
 )
 
 type CdkStackProps struct {
@@ -17,9 +26,26 @@ func NewCdkStack(scope constructs.Construct, id string, props *CdkStackProps) aw
 		sprops = props.StackProps
 	}
 	stack := awscdk.NewStack(scope, &id, &sprops)
-	awss3.NewBucket(stack, jsii.String("scribe-videos"), &awss3.BucketProps{
-		Versioned: jsii.Bool(true),
+
+	// timestamp needed because bucket name has to be globally unique
+	timestamp := time.Now().Unix()
+	timestampString := strconv.Itoa(int(timestamp))
+	awss3.NewBucket(stack, jsii.String("scribe-videos-"+timestampString), &awss3.BucketProps{})
+
+	snsTopic := awssns.NewTopic(stack, jsii.String("scribe-topic"), &awssns.TopicProps{
+		DisplayName:               jsii.String("scribe-topic"),
+		ContentBasedDeduplication: jsii.Bool(true),
+		Fifo:                      jsii.Bool(true),
+		TopicName:                 jsii.String("scribe-topic"),
 	})
+
+	sqsQueue := awssqs.NewQueue(stack, jsii.String("scribe-audio-to-process"), &awssqs.QueueProps{
+		QueueName:                 jsii.String("scribe-audio-to-process.fifo"),
+		ContentBasedDeduplication: jsii.Bool(true),
+		Fifo:                      jsii.Bool(true),
+	})
+
+	snsTopic.AddSubscription(awssnssubscriptions.NewSqsSubscription(sqsQueue, &awssnssubscriptions.SqsSubscriptionProps{}))
 
 	return stack
 }
@@ -49,9 +75,17 @@ func env() *awscdk.Environment {
 	// Uncomment if you know exactly what account and region you want to deploy
 	// the stack to. This is the recommendation for production stacks.
 	//---------------------------------------------------------------------------
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("Error loading .env file: %s", err)
+	}
+
+	account := os.Getenv("ACCOUNT")
+	region := os.Getenv("REGION")
 	return &awscdk.Environment{
-		Account: jsii.String("ACCOUNT"),
-		Region:  jsii.String("REGION"),
+		Account: jsii.String(account),
+		Region:  jsii.String(region),
 	}
 
 	// Uncomment to specialize this stack for the AWS Account and Region that are
--- server/cdk/go.mod
+++ server/cdk/go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/cdklabs/awscdk-asset-kubectl-go/kubectlv20/v2 v2.1.2 // indirect
 	github.com/cdklabs/awscdk-asset-node-proxy-agent-go/nodeproxyagentv6/v2 v2.0.1 // indirect
 	github.com/fatih/color v1.16.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/yuin/goldmark v1.4.13 // indirect
