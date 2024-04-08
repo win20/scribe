51413f90d07e2ae8c2c94625875c9c87c151c42c
Win Barua <win.barua2@gmail.com>
2024-04-08 19:51:56 +0100

    add aws config

---
+++ server/service/aws.go
@@ -0,0 +1,18 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+)
+
+func LoadConfig() aws.Config {
+	config, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		fmt.Println("Error loading aws sdk: ", err)
+	}
+
+	return config
+}
